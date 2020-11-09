11decdb92b0b29f2b43c5141a9558e1e61a197b3
EsterTampubolon <romaaatampubolon@gmail.com>
2020-11-09 07:43:20 -0800

    exercise 07

---
+++ pertemuan_23/index.js
@@ -0,0 +1,83 @@
+// // //variabel
+
+// // //let&const
+
+// // let umur = 25;
+// // umur = 26;
+// // let nilai = 95.5;
+// // let sudah menikah = true;
+// // let nama = "john";
+// // console.log(umur);
+// // console.log(nama);
+
+// // let alamat;
+// // console.log(alamat)
+
+// // const tahunlahir =2000;
+// // console.log(tahunlahir);
+
+
+// //operator
+
+// //arithematic operator (+,-,/,*)
+
+// let angka1 =10;
+// let angka2 =20;
+// let hasil = angka1 + angka2;
+// console.log(hasil);
+
+// let umur = 30;
+// let namadepan ="john";
+// let namabelakang ="doe";
+// console.log(
+//     "nama saya" +" " +
+//      namadepan+
+//      " "+
+//      namabelakang +
+//      ". umur saya adalah " +
+//      umur +
+//      " tahun"
+//      );
+
+// operator perbandingan
+
+// let angka1 =10;
+// let angka2 =10;
+
+// let hasil = angka1 <= angka2;
+// console.log(hasil1);
+
+//ternary operator
+
+// let angka =5;
+// let hasil = angka <=0? "negatif": "positif"
+// console.log(hasil)
+
+//operator logika
+// let kondisi1= 1<2;
+// let kondisi2= false;
+
+// console.log(kondisi1 || kondisi2);
+
+// console.log(kondisi1)
+
+
+// let angka = prompt("masukan angka:");
+// console.log("angka yang dimasukan adalah: " + angka);
+
+//exercise 7
+//menghitung bmi
+
+let berat= prompt("Masukan berat: ");
+console.log("Berat adalah:" +berat)
+
+let tinggi= prompt("Masukan Tinggi: ");
+console.log("Tinggi adalah:" +tinggi);
+
+let bilangan1=berat;
+let bilangan2=tinggi;
+
+console.log(bilangan1/bilangan2*bilangan2);
+
+
+
